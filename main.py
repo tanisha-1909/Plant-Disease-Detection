eebabf61ab068d11fdba6833f96b58b2d26a1ed4
tanisha-1909 <tanishamahavar32@gmail.com>
2025-04-06 04:58:51 +0530

    path of keras

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ st.markdown("""
 
 
 def model_prediction(test_image):
-    model = tf.keras.models.load_model('trained_model.keras')
+    model = tf.keras.models.load_model("trained_model.keras")
     image = tf.keras.preprocessing.image.load_img(test_image, target_size=(128, 128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr])
